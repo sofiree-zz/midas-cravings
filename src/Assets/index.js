f8f075077e847a08b5a8a4d82551f30b60c65ec8
sofiree <muosophia0@gmail.com>
2021-11-07 21:02:45 +0100

    cleared index.js/assets

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -1,8 +1,3 @@
 import Logo from "./Images/Logo.svg";
-import Tacoshop from "./Images/Tacoshop.jpg";
-import Aboutus from "./Images/Aboutus.jpg";
-import Burger from "./Images/Burger.jpg";
-import Burgerman from "./Images/Burgerman.jpg";
-import Coffeetable from "./Images/Coffeetable.jpg";
-import Cocktail from "./Images/Cocktail.jpg";
-export {Logo, Tacoshop, Aboutus, Burger, Burgerman, Cocktail, Coffeetable};
+
+export {Logo};
