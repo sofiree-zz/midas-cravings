3caa225d775992b95c3b0f087c78830115fe295b
Sofiree <muosophia0@gmail.com>
2021-11-25 15:31:48 +0100

    changes to footer css

--- src/Widgets/Footer/Footer.js
+++ src/Widgets/Footer/Footer.js
@@ -3,7 +3,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <>
-    {/* fixed-bottom */}
+      {/* fixed-bottom */}
       <footer className="page-footer " id="footer-layout">
         <div id="footer-holder">
           <div className="container-fluid  text-md-left" id="footer-container">
@@ -11,22 +11,26 @@ const Footer = () => {
               <section className="col">
                 <h6>Sunday-Thursday</h6>
                 <span>4-9pm</span>
-            
+
                 <h6>Friday-Sunday</h6>
                 <span>4-10pm</span>
               </section>
               <hr className="col" id="footer-divider" />
-              <section className="col" >
+              <section className="col">
                 <h6>09055506102</h6>
                 <h6>7 Achi Street Independence Layout, Enugu. 400001.</h6>
               </section>
             </div>
-            <div className="" id="footer-container2">
-              <section>
-                <h6>FOLLOW</h6>
-                <i className="fa fa-instagram fa-lg" aria-hidden="true"></i>
-                <h6>GET THE LATEST NEWS. SIGN UP FOR OUR NEWSLETTER.</h6>
-                <p className="footer-copyright">© Midas Cravings 2021</p>
+            <div id="footer-container2">
+              <section className="row" id="footer-copyright">
+
+                <div className="col">
+                  <h6>FOLLOW</h6>
+                  <i className="fa fa-instagram fa-lg" aria-hidden="true"></i>
+                  <h6>GET THE LATEST NEWS. SIGN UP FOR OUR NEWSLETTER.</h6>
+                </div>
+
+                <p className="col">© Midas Cravings 2021</p>
               </section>
             </div>
           </div>
